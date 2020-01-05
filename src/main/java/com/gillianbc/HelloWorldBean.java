7190864df3b51cdcb4e4b675b5df1ae708d60138
gillianbc <gszemeti@gmail.com>
2020-01-05 15:43:37 +0000

    GET, GET with bean

---
+++ src/main/java/com/gillianbc/HelloWorldBean.java
@@ -0,0 +1,77 @@
+package com.gillianbc;
+
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
+
+public class HelloWorldBean {
+	private String message;
+	private final int age;
+	private final Date date;
+	List<String> list = new LinkedList<String>();
+	private String id;
+
+	public HelloWorldBean(String message) {
+		super();
+		this.message = message;
+		this.date = new Date();
+		this.age = 27;
+		list.add("elephant");
+		list.add("frog");
+
+	}
+	
+	
+
+	public HelloWorldBean(String message, String id) {
+		this(message);
+		this.id = id;
+	}
+
+
+
+	public String getId() {
+		return id;
+	}
+
+
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+
+
+	public List<String> getList() {
+		return list;
+	}
+
+	public void setList(List<String> list) {
+		this.list = list;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	@Override
+	public String toString() {
+		return "HelloWorldBean [message=" + message + ", age=" + age + ", date=" + date + "]";
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+
+
+
+}
---
+++ src/main/java/com/gillianbc/HelloWorldController.java
@@ -0,0 +1,49 @@
+package com.gillianbc;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+	//First way of doing a GET
+	@RequestMapping(method=RequestMethod.GET, path="hello")
+	public String helloWorld() {
+		return "hello world";
+	}
+
+	//Better way of doing a GET
+	@GetMapping(path="bye")
+	public String byebye() {
+		return "bye bye bye";
+	}
+
+	/*
+	 For this to work i.e. return a json representation of the object,
+	 the class must have a getter for at least one of its propertues.
+	 Without a getter, you will get error:
+	 No converter found for return value of type: class com.gillianbc.HelloWorldBean
+	 The json response will use the available getters
+	 It's Jackson and auto-config that do the conversion to JSON
+	 Add the JSON Viewer Chrome extension to see it nicely formatted
+	 */
+	// Getting a bean
+	@GetMapping(path="bean")
+	public HelloWorldBean beany() {
+		return new HelloWorldBean("I am a bean");
+	}
+
+	
+	// Getting a bean with a param
+	@GetMapping(path = "bean/{id}")
+	public HelloWorldBean beany2(@PathVariable	String id) {
+		return new HelloWorldBean("I am bean too ", id) ;
+	}
+
+
+}
+
+
