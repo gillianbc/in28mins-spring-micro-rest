65ea136ebd9c9c505a60ad3c43e2cfad8c558cc1
gillianbc <gszemeti@gmail.com>
2020-04-11 18:05:43 +0100

    Fix swagger api-docs json rendering issue - chrome ok, safari still not ok

--- src/main/java/com/gillianbc/RestfulWebServicesApplication.java
+++ src/main/java/com/gillianbc/RestfulWebServicesApplication.java
@@ -1,14 +1,19 @@
 package com.gillianbc;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
 
 @SpringBootApplication
 public class RestfulWebServicesApplication {
@@ -31,6 +36,17 @@ public class RestfulWebServicesApplication {
 		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
 		messageSource.setBasename("message");
 		return messageSource;
+	}
+	
+	@Bean
+	public RequestMappingHandlerAdapter requestHandler() {
+	    RequestMappingHandlerAdapter adapter = new RequestMappingHandlerAdapter();
+	    MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+	    List<MediaType> mediaTypeList = new ArrayList<>();
+	    mediaTypeList.add(MediaType.APPLICATION_JSON);
+	    converter.setSupportedMediaTypes(mediaTypeList);
+	    adapter.getMessageConverters().add(converter);
+	    return adapter;
 	}
 
 }
--- src/main/java/com/gillianbc/SwaggerConfig.java
+++ src/main/java/com/gillianbc/SwaggerConfig.java
@@ -15,4 +15,5 @@ public class SwaggerConfig {
 	public Docket api() {
 		return new Docket(DocumentationType.SWAGGER_2);
 	}
+	
 }
